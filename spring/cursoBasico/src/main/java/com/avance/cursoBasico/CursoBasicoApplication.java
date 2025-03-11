d835dc2f5346e1a2cf5a32271ed974ab21f78b23
juandgg <juanguiza65@gmail.com>
2025-03-11 10:32:02 -0500

    add file to repository

---
+++ spring/cursoBasico/src/main/java/com/avance/cursoBasico/CursoBasicoApplication.java
@@ -0,0 +1,13 @@
+package com.avance.cursoBasico;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CursoBasicoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CursoBasicoApplication.class, args);
+	}
+
+}
---
+++ spring/cursoBasico/src/test/java/com/avance/cursoBasico/CursoBasicoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.avance.cursoBasico;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CursoBasicoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
