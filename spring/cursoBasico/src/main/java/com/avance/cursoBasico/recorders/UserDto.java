6e7cf90292e84bd63874a3622d5536d65c283d8b
juandgg <juanguiza65@gmail.com>
2025-03-12 17:57:44 -0500

    feat:tada:se agregan archivos repaso basico spring boot

---
+++ spring/cursoBasico/src/main/java/com/avance/cursoBasico/recorders/UserDto.java
@@ -0,0 +1,8 @@
+package com.avance.cursoBasico.recorders;
+
+/*
+* un recorder se integra en versiones superiores de java pero es una forma de
+* usar como una especie de lombok en clases si tener que usar lombok
+* */
+public record UserDto(String name,int age) {
+}
